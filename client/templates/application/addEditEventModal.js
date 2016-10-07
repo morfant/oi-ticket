5c6d5db4cfeb06440f3f6053a7ecbfcfdeea1741
morfant <giy.hands@gmail.com>
2016-10-07 18:45:38 +0900

    Calendering...

---
+++ client/templates/application/addEditEventModal.js
@@ -0,0 +1,97 @@
+var closeModal = () => {
+  $( '#add-edit-event-modal' ).modal( 'hide' );
+  $( '.modal-backdrop' ).fadeOut();
+};
+
+
+Template.addEditEventModal.helpers({
+  modalType( type ) {
+    let eventModal = Session.get( 'eventModal' );
+    if ( eventModal ) {
+      return eventModal.type === type;
+    }
+  },
+  modalLabel() {
+    let eventModal = Session.get( 'eventModal' );
+
+    if ( eventModal ) {
+      return {
+        button: eventModal.type === 'edit' ? 'Edit' : 'Add',
+        label: eventModal.type === 'edit' ? 'Edit' : 'Add an'
+      };
+    }
+  },
+  selected( v1, v2 ) {
+    return v1 === v2;
+  },
+  event() {
+    let eventModal = Session.get( 'eventModal' );
+
+    if ( eventModal ) {
+
+      return eventModal.type === 'edit' ? Events.findOne( eventModal.event ) : {
+        start: eventModal.start,
+        end: eventModal.end
+      };
+    }
+  }
+});
+
+
+// Template.postSubmit.events({
+
+Template.addEditEventModal.events({
+
+  'click #submitButton': function( e, template ) {
+    e.preventDefault();
+
+    var eventModal = Session.get( 'eventModal' ),
+        submitType = eventModal.type === 'edit' ? 'editEvent' : 'addEvent',
+        eventItem  = {
+          title: template.find( '[name="title"]' ).value,
+          start: template.find( '[name="start"]' ).value,
+          end: template.find( '[name="end"]' ).value,
+          type: template.find( '[name="type"] option:selected' ).value
+          // guests: parseInt( template.find( '[name="guests"]' ).value, 10 )
+        };
+
+    if (eventItem.type == 'Live') eventItem.backgroundColor = "rgb(201, 48, 44)";
+    if (eventItem.type == 'Recorded') eventItem.backgroundColor = "rgb(58, 135, 173)";
+
+    if ( submitType === 'editEvent' ) {
+      eventItem._id   = eventModal.event;
+    }
+
+    Meteor.call( submitType, eventItem, ( error ) => {
+      if ( error ) {
+        Bert.alert( error.reason, 'danger' );
+      } else {
+        Bert.alert( `Event ${ eventModal.type }ed!`, 'success' );
+        closeModal();
+      }
+    });
+  },
+
+  'click .addShow': function( e ) {
+    e.preventDefault();
+    console.log("plus button clicked!");
+
+  },
+
+  'click .delete-event': function( e ) {
+    e.preventDefault();
+
+    let eventModal = Session.get( 'eventModal' );
+    if ( confirm( 'Are you sure? This is permanent.' ) ) {
+      Meteor.call( 'removeEvent', eventModal.event, function( error ) {
+        if ( error ) {
+          Bert.alert( error.reason, 'danger' );
+        } else {
+          Bert.alert( 'Event deleted!', 'success' );
+          closeModal();
+        }
+      });
+    }
+  }
+
+});
---
+++ client/templates/application/calender.js
@@ -0,0 +1,156 @@
+var MOMENT_FORMAT = "MMM D, YYYY HH:mm";
+var MOMENT_FORMAT_DAY = "YYYY MMMM Do";
+
+var isPast = ( date ) => {
+  let today = moment().format(MOMENT_FORMAT);
+  return moment( today ).isAfter( date );
+};
+
+
+Template.calender.created = function() {
+  let template = Template.instance();
+  template.subscribe( 'events' );
+}
+
+Template.calender.helpers({
+
+
+});
+
+Template.calender.events({
+
+
+ 
+});
+
+Template.calender.rendered = function() {
+
+  $('#kalendar').fullCalendar({
+    defaultView: 'month', //agendaWeek,agendaDay
+
+    selectable: true,
+
+    height: 400,
+    aspectRatio: 1.5,
+    editable: true,
+    // contentHeight: 300,
+
+    // events: [
+    //     {
+    //         title: 'My Event',
+    //         start: '2016-07-29',
+    //         url: 'http://google.com/'
+    //     }
+    //     // other events here
+    // ],
+    // resources: [
+    //     { id: 'a', title: 'Room A' },
+    //     { id: 'b', title: 'Room B' },
+    //     { id: 'c', title: 'Room C' },
+    //     { id: 'd', title: 'Room D' }
+    // ],
+    // customButtons: {
+    //     // myCustomButton: {
+    //     //     text: 'custom!',
+    //     //     click: function() {
+    //     //         alert('clicked the custom button!');
+    //     //     }
+    //     // }
+    // },
+    header: {
+        left: 'prev,next',
+        center: 'title',
+        right: 'today myCustomButton'
+    },
+    customButtons: {
+        myCustomButton: {
+            text: 'custom!',
+            click: function() {
+                alert('clicked the custom button!');
+            }
+        }
+    },    
+    views: {
+        month: { // name of view
+            // titleFormat: 'YYYY, MM, DD'
+            // other view-specific options here
+        }
+    },
+    events( start, end, timezone, callback ) {
+      let data = Events.find().fetch().map( ( event ) => {
+        // event.eventStartEditable = !isPast( event.start );
+        return event;
+      });
+
+      if ( data ) {
+        callback( data );
+      }
+    },
+    eventRender( event, element ) { //Triggered while an event is being rendered.
+      element.find( '.fc-content' ).html(
+        `<h4>${ event.title }</h4>
+         <p class="type-${ event.type }">#${ event.type }</p>
+        `
+      );
+    },
+    // eventDrop( event, delta, revert ) {
+    //   let _start = event.start.format(MOMENT_FORMAT);
+    //   let _end = event.end.format(MOMENT_FORMAT);
+    //   if ( !isPast( _start ) ) {
+    //     let update = {
+    //       _id: event._id,
+    //       start: _start,
+    //       end: _end
+    //     };
+
+    //     Meteor.call( 'editEvent', update, ( error ) => {
+    //       if ( error ) {
+    //         Bert.alert( error.reason, 'danger' );
+    //       }
+    //     });
+    //   } else {
+    //     revert();
+    //     Bert.alert( 'Sorry, you can\'t move items to the past!', 'danger' );
+    //   }
+    // },
+    // dayClick( date ) {
+    //   Session.set( 'eventModal', {
+    //     type: 'add',
+    //     start: date.add(5, 'hour').format(MOMENT_FORMAT),
+    //     end: date.add(2,'hour').format(MOMENT_FORMAT)
+    //   });
+    //   // console.log(date);
+    //   // console.log(date.format("dddd, YYYY MM DD, h:mm a"));
+    //   $( '#add-edit-event-modal' ).modal( 'show' );
+    // },
+    eventClick( event ) {
+      Session.set( 'eventModal', { type: 'edit', event: event._id } );
+      $( '#add-edit-event-modal' ).modal( 'show' );
+    },
+    select( start, end, jsEvent, view ) {
+      console.log("select!");
+      console.log(start);
+      console.log(end);
+      // console.log(jsEvent);
+      // console.log(view);
+      Session.set( 'eventModal', {
+        type: 'add',
+        start: start.format(MOMENT_FORMAT_DAY),
+        end: end.format(MOMENT_FORMAT_DAY)
+      });
+      // console.log(date);
+      // console.log(date.format("dddd, YYYY MM DD, h:mm a"));
+      $( '#add-edit-event-modal' ).modal( 'show' );
+    }
+
+
+  });
+
+
+  Tracker.autorun( function() {
+    Events.find().fetch();
+    $( '#kalendar' ).fullCalendar( 'refetchEvents' );
+  });
+
+
+}
