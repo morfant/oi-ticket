6df84b39e967b3605b803ec81326a0397219ffed
morfant <giy.hands@gmail.com>
2016-10-08 15:42:18 +0900

    Add time select

--- client/templates/include/aShowInput.js
+++ client/templates/include/aShowInput.js
@@ -1,6 +1,21 @@
 Template.aShowInput.created = function() {
     showIndex = 0;
 
+    hours = [];
+    mins = [];
+
+    for (var i = 0; i <= 24; i++){
+        if (i < 10) hours[i] = "0"+i;
+        else hours[i] = i.toString();
+    }
+    console.log("hours: " + hours);
+
+    for (var i = 1; i <= 6; i++){
+        mins[0] = "00";
+        mins[i] = i * 10;
+    }
+    console.log("mins: " + mins);
+
 };
 
 
@@ -24,10 +39,16 @@ Template.aShowInput.helpers({
         return showIndex + " 회차";
     },
     nShowId() {
-        return "modal_input_" + showIndex;
+        return "modal_show_" + showIndex;
     },
     nShowDelBtnClass() {
         return "modal_input_delBtn_" + showIndex;
+    },
+    getHours() {
+        return hours;
+    },
+    getMins() {
+        return mins;
     }
   
 
