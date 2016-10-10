2278fa92eb4716d8e3b27e5d0ce7503e276e0b2c
morfant <giy.hands@gmail.com>
2016-10-10 12:00:35 +0900

    Implementing calender in submit page.

--- client/templates/application/addEditEventModal.js
+++ client/templates/application/addEditEventModal.js
@@ -77,35 +77,70 @@ Template.addEditEventModal.events({
     var eventModal = Session.get( 'eventModal' ),
         submitType = eventModal.type === 'edit' ? 'editEvent' : 'addEvent';
 
-    var eventItems  = [];
+    var showDays  = [];
     var days = eventModal.days;
 
     for (var j = 0; j < days.length; j++) {
-      var eventItem = {};
+      var aDayItem = {};
       console.log("days " + j + " :" + days[j]);
-      eventItem.date = days[j];
-      var times = [];
+      aDayItem.date = days[j];
+      aDayItem.available = true;
+      var shows = [];
       for (var i = 0; i < nShow; i++) {
+        var aShow = {};
         var h_id = "#hour_modal_show_" + (i + 1);
         var m_id = "#min_modal_show_" + (i + 1);
         var gotHour = template.find(h_id).value;
         var gotMin = template.find(m_id).value;
-        var time = gotHour + gotMin;
-        console.log("time: " + time);
-        // console.log("id: " + id);
-        times[i] = time;
-        // times[i] = template.find('#hour_modal_show_1').value;
-        console.log(times[i]);
+        var gotTime = gotHour + ":" + gotMin;
+        var maxSeats = template.find("#modal_num_of_seats").value;
+        aShow.time = gotTime;
+        // aShow.title = (i+1) + " 회차";
+        // aShow.start = days[j] + " " + gotTime;
+        aShow.event = {
+          title: (i+1) + " 회차", // + gotTime
+          start: days[j] + " " + gotTime, //http://momentjs.com/docs/#/parsing/string/, 
+          // 2013-02-08 09:30 
+          seats: maxSeats
+        };
+        console.log("time + " + i + ": " + gotTime);
+        shows[i] = aShow;
       }
-      eventItem.time = times;
-
-      eventItems[j] = eventItem;
-      console.log("eventItem.date: " + eventItem.date);
-      console.log("eventItem.time: " + eventItem.time);
-      console.log("eventItem " + j + " :" + eventItems[j]);
+      aDayItem.shows = shows;
+      // aDayItem.title = "회차";
+      // aDayItem.start = "2016-10-12 17:00";
+      showDays[j] = aDayItem;
     }
 
-    // console.log("eventItems: " + eventItems);
+    /* print object values */
+    // for (var i = 0; i < days.length; i++) {
+    //   console.log("eventItems["+i+"].date: " + eventItems[i].date);
+    //   for (var j = 0; j < nShow; j++) {
+    //     console.log("eventItems["+i+"].time["+j+"]: " + eventItems[i].time[j]);
+    //   }
+    // }
+
+    if ( submitType === 'editEvent' ) {
+      showDays._id   = eventModal.event;
+    }
+
+
+    _.each(showDays, function(eventItem) {
+
+      Meteor.call( submitType, eventItem, ( error, result ) => {
+        if ( error ) {
+          Bert.alert( error.reason, 'danger' );
+        } else {
+          console.log("Input events succeed!");
+          console.log("result: " + result);
+          // Bert.alert( `Event ${ eventModal.type }ed!`, 'success' );
+          Bert.alert( `Event input succeed!`, 'success' ); //Not working...?
+          closeModal();
+        }
+      });
+
+    });
+
 
     // type: template.find( '[name="type"] option:selected' ).value
     // guests: parseInt( template.find( '[name="guests"]' ).value, 10 )
@@ -115,9 +150,6 @@ Template.addEditEventModal.events({
     // if (eventItem.type == 'Live') eventItem.backgroundColor = "rgb(201, 48, 44)";
     // if (eventItem.type == 'Recorded') eventItem.backgroundColor = "rgb(58, 135, 173)";
 
-    if ( submitType === 'editEvent' ) {
-      eventItem._id   = eventModal.event;
-    }
 
     // Meteor.call( submitType, eventItem, ( error ) => {
     //   if ( error ) {
--- client/templates/application/calender.js
+++ client/templates/application/calender.js
@@ -1,5 +1,5 @@
 var MOMENT_FORMAT = "MMM D, YYYY HH:mm";
-var MOMENT_FORMAT_DAY = "YYYY MMMM Do";
+var MOMENT_FORMAT_DAY = "YYYY-MM-DD";
 
 var isPast = ( date ) => {
   let today = moment().format(MOMENT_FORMAT);
@@ -77,21 +77,95 @@ Template.calender.rendered = function() {
         }
     },
     events( start, end, timezone, callback ) {
-      let data = Events.find().fetch().map( ( event ) => {
+
+      var events = [];
+      Events.find().fetch().map( ( event ) => {
+
+        // console.log("event: " + event);
+        var n = event.shows.length;
+        for (var i = 0; i < n; i++) {
+          events.push(event.shows[i].event);
+          console.log("event start time in events(): " + event.shows[i].event.start);
+        }
+
         // event.eventStartEditable = !isPast( event.start );
-        return event;
+        // return event;
       });
 
-      if ( data ) {
-        callback( data );
+      if ( events ) {
+        // console.log("events: " + events);
+        callback( events );
       }
     },
     eventRender( event, element ) { //Triggered while an event is being rendered.
+
+      console.log("eventRender() event.title: " + event.title);
+      console.log("eventRender() event.start: " + event.start.format('HH:mm'));
+
+      var timeStr = event.start.format("HH:mm");
+
+/*
+      var titles = [];
+      var starts = [];
+      var showClass = [];
+      var fcElements = [];
+      for (var i = 0; i < event.shows.length; i++) {
+        titles.push(event.shows[i].event.title);
+        starts.push(event.shows[i].event.start);
+        showClass = "show_" + (i+1);
+
+        var fcOriginal = element.find('.fc-event-container').html();
+        console.log(fcOriginal);
+        var fce = document.createElement("div"); 
+        fce.className = "fc-content" + " showtime_" + (i+1);
+        fce.innerHTML = fcOriginal;
+        fcElements.push(fce);
+        // console.log("fceElement: " + fcElements[i]);
+
+      };
+
+*/
+
+
+  // var newContent = document.createTextNode("Hi there and greetings!"); 
+  // newDiv.appendChild(newContent); //add the text node to the newly created div. 
+
+
+   // var x = document.getElementById("myLI").parentElement.nodeName;
+
+
+
+// <a class="fc-day-grid-event fc-h-event fc-event fc-start fc-end fc-draggable"><div class="fc-content"><span class="fc-time">10:30a</span> <span class="fc-title">Meeting</span></div></a>
+
+/*
+      var fcParent = element.find('.fc-content').parent();
+
+      for (var i = 0; i < event.shows.length; i++) {
+        fcElements[i].innerHTML = 
+          "<h4>" + titles[i] + "</h4> \
+          <p class=" + showClass + ">" + starts[i] + "</p>"
+          ;
+*/
+        // element.find("showtime_"+(i+1)).html(
+        //   "<h4>" + titles[i] + "</h4> \
+        //   <p class=" + showClass + ">" + starts[i] + "</p>"
+        //   );
+
+        // fcParent.append(fcElements[i]);
+
+
+        // element.find( '.fc-content' ).html(
+          // `<h4>${ event.title }</h4>
+          // "<h4>" + titles[i] + "</h4> \
+          // <p class=" + showClass + ">" + starts[i] + "</p>"
+        // );
+// <span class="fc-time">7a</span> <span class="fc-title">Birthday Party</span>
+
       element.find( '.fc-content' ).html(
-        `<h4>${ event.title }</h4>
-         <p class="type-${ event.type }">#${ event.type }</p>
-        `
+        "<span class=\"fc-time\">" + timeStr + "</span> \
+         <span class=\"fc-title\">" + event.title + "</span>"
       );
+
     },
     // eventDrop( event, delta, revert ) {
     //   let _start = event.start.format(MOMENT_FORMAT);
@@ -133,7 +207,7 @@ Template.calender.rendered = function() {
       // console.log(end);
       var days = [];
 
-      for(var iMoment = start; iMoment.isBefore(end); iMoment.add(1, 'days')){
+      for(var iMoment = start.clone(); iMoment.isBefore(end); iMoment.add(1, 'days')){
         var aMoment = iMoment.clone().format(MOMENT_FORMAT_DAY); 
         // console.log("amoment: " + aMoment);
         days.push(aMoment);
@@ -146,7 +220,7 @@ Template.calender.rendered = function() {
         type: 'add',
         start: start.format(MOMENT_FORMAT_DAY),
         days: days,
-        end: end.format(MOMENT_FORMAT_DAY)
+        end: end.subtract(1, 'days').format(MOMENT_FORMAT_DAY)
       });
       // console.log(date);
       // console.log(date.format("dddd, YYYY MM DD, h:mm a"));
